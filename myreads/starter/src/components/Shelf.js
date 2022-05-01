de9d8fc30316bc3a0f913dc1687a63f77e46b949
David Rajcher (Codaisseur) <davidrajchercodaisseur@Davids-MacBook-Pro.local>
2022-05-01 22:31:38 +0200

    changed set functions and used API

--- myreads/starter/src/components/Shelf.js
+++ myreads/starter/src/components/Shelf.js
@@ -1,28 +1,55 @@
-import Book from "./Book.js";
+import Book from "./Book";
 
-const Shelf = ({name, updateCurrentlyReading, updateWantToRead, updateRead, deleteBook, list, currentShelf, updateCurrentShelf}) => {
-    
-    return(
-        <div className="bookshelf">
-            <h2 className="bookshelf-title">{name}</h2>
-            <div className="bookshelf-books">
-                <ol className="books-grid">
-                {list ? list.map((book) => 
-                <Book   
+const Shelf = ({
+  name,
+  updateCurrentlyReading,
+  updateWantToRead,
+  updateRead,
+  deleteBook,
+  list,
+  currentShelf,
+  updateCurrentShelf,
+  shelf,
+}) => {
+  console.log({
+    name,
+    updateCurrentlyReading,
+    updateWantToRead,
+    updateRead,
+    deleteBook,
+    list,
+    currentShelf,
+    updateCurrentShelf,
+    shelf,
+  });
+
+  return (
+    <div className='bookshelf'>
+      <h2 className='bookshelf-title'>{name}</h2>
+      <div className='bookshelf-books'>
+        <ol className='books-grid'>
+          {list ? (
+            list.map((book) => (
+              <Book
                 key={book.id}
-                title={book.title} 
-                authors={book.authors} 
+                title={book.title}
+                authors={book.authors}
                 image={book.imageLinks.thumbnail}
                 currentShelf={currentShelf}
-                updateCurrentShelf={() => updateCurrentShelf()}
+                updateCurrentShelf={() => updateCurrentShelf(shelf)}
                 deleteBook={() => deleteBook(book)}
                 updateCurrentlyReading={() => updateCurrentlyReading(book)}
                 updateWantToRead={() => updateWantToRead(book)}
-                updateRead={() => updateRead(book)}/>) : <p>No Books to show</p>}
-                </ol>
-            </div>
-        </div>
-    )
+                updateRead={() => updateRead(book)}
+              />
+            ))
+          ) : (
+            <p>No Books to show</p>
+          )}
+        </ol>
+      </div>
+    </div>
+  );
 };
 
 export default Shelf;
--- myreads/starter/src/views/Search.js
+++ myreads/starter/src/views/Search.js
@@ -1,69 +1,81 @@
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { search } from "../BooksAPI.js";
-import Book from "../Components/Book.js";
+import { search } from "../BooksAPI";
+import Book from "../components/Book";
 
-const Search = ({deleteBook,  updateCurrentlyReading, updateWantToRead, updateRead, currentShelf, updateCurrentShelf}) => {
-
+const Search = ({
+  deleteBook,
+  updateCurrentlyReading,
+  updateWantToRead,
+  updateRead,
+  currentShelf,
+  updateCurrentShelf,
+}) => {
   //get data from api
   const [showSearch, setShowSearch] = useState([]);
   const [searchValue, setSearchValue] = useState("");
 
   const updateSearchValue = (searchValue) => {
-    setSearchValue(searchValue)
+    setSearchValue(searchValue);
   };
 
   useEffect(() => {
-
     let mounted = true;
 
-    if(searchValue !== ""){
+    if (searchValue !== "") {
       search(searchValue, 20)
-      .then(items => {
-        if(mounted) {
-          setShowSearch(items)
-        }
-      })
-      .catch((error) => console.log(error))
+        .then((items) => {
+          if (mounted) {
+            setShowSearch(items);
+          }
+        })
+        .catch((error) => console.log(error));
     }
-    if(searchValue === ""){setShowSearch([])}
-      
-  return () => mounted = false;
-  }, [searchValue])
-  
+    if (searchValue === "") {
+      setShowSearch([]);
+    }
 
-  return(
-     <div className="search-books">
-       <div className="search-books-bar">
-         <Link to="/" className="close-search">
-           Close
-         </Link>
-         <div className="search-books-input-wrapper">
+    return () => (mounted = false);
+  }, [searchValue]);
+
+  return (
+    <div className='search-books'>
+      <div className='search-books-bar'>
+        <Link to='/' className='close-search'>
+          Close
+        </Link>
+        <div className='search-books-input-wrapper'>
           <input
-            type="text"
-            placeholder="Search by title, author, or ISBN"
+            type='text'
+            placeholder='Search by title, author, or ISBN'
             value={searchValue}
             onChange={(event) => updateSearchValue(event.target.value)}
           />
         </div>
       </div>
-      <div className="search-books-results">
-         <ol className="books-grid">
-          {showSearch.length>0 ? showSearch.map((book) => 
-          <Book 
-          key={book.id}
-          title={book.title} 
-          authors={book.authors} 
-          image={book.imageLinks.thumbnail}
-          currentShelf={currentShelf}
-          updateCurrentShelf={(shelf) => updateCurrentShelf(shelf)}
-          deleteBook={() => deleteBook()}
-          updateCurrentlyReading={() => updateCurrentlyReading(book)}
-          updateWantToRead={() => updateWantToRead(book)}
-          updateRead={() => updateRead(book)}/>) : <p>No Books to show</p>}
-         </ol>
-       </div>
-     </div>
-  )
-}
+      <div className='search-books-results'>
+        <ol className='books-grid'>
+          {showSearch.length > 0 ? (
+            showSearch.map((book) => (
+              <Book
+                key={book.id}
+                title={book.title}
+                authors={book.authors}
+                image={book.imageLinks.thumbnail}
+                currentShelf={currentShelf}
+                updateCurrentShelf={(shelf) => updateCurrentShelf(shelf)}
+                deleteBook={() => deleteBook()}
+                updateCurrentlyReading={() => updateCurrentlyReading(book)}
+                updateWantToRead={() => updateWantToRead(book)}
+                updateRead={() => updateRead(book)}
+              />
+            ))
+          ) : (
+            <p>No Books to show</p>
+          )}
+        </ol>
+      </div>
+    </div>
+  );
+};
 export default Search;
